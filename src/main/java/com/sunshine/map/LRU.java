7b8e3d78991b912558a8be608845621cc19c568f
王涛 <wangtao@fxiaoke.com>
2018-04-19 14:46:32 +0800

    lru cache

--- src/main/java/com/sunshine/map/LRU.java
+++ src/main/java/com/sunshine/map/LRU.java
@@ -1,25 +1,35 @@
 package com.sunshine.map;
 
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Created by wangtao on 2018/4/8.
  */
-public class LRU<K, V> {
+public class LRU<K, V> extends LinkedHashMap<K, V> {
 
-    private LinkedHashMap<K, V> linkedHashMap;
+    private final int maxSize;
 
-
-
-    public LRU() {
-        linkedHashMap = new LinkedHashMap<>();
+    public LRU(int maxSize) {
+        this(maxSize, 16, 0.75f, false);
     }
 
-    public void put(K key, V value) {
-
-        linkedHashMap.put(key, value);
-
+    public LRU(int maxSize, int initialCapacity, float loadFactory, boolean accessOrder) {
+         super(16, 0.75f, accessOrder);
+         this.maxSize = maxSize;
     }
 
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+        return size() > maxSize;
+    }
 
+    public static void main(String[] args) {
+        LRU<String, String> lru = new LRU<String, String>(10);
+        for (int i = 0; i < 20; i++) {
+            lru.put(i + "", "i");
+            System.out.println(lru);
+        }
+        
+    }
 }
